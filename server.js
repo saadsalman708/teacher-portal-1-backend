0c0c954a6628af7692cbc93cb64d0bdea95d405b
Saad Salman <saadsalman708@gmail.com>
2026-05-20 02:43:03 +0500

    change file name & compeleted basic structure and connected post.route.js

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
+import router from "./src/routes/post.routes.js";
 import "dotenv/config";
-
 import connectDB from "./src/config/db.js"
 
 
 const PORT = process.env.PORT || 5000;
+
 connectDB();
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/" , (req , res)=> {
+app.use(router , (req , res)=> {
     res.status(200).json("server says hi!");
 });
 
