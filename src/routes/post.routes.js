1271a6d134c5a14b70a2a3634c754dc9d4d6f78c
Saad Salman <saadsalman708@gmail.com>
2026-05-20 03:29:46 +0500

    fixed the bug on post.routes.js

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import { get, create, remove } from "../controllers/post.controller";
+import { getPosts, create, remove } from "../controllers/post.controller";
 
 router.post("/", create);
-router.get("/", get);
+router.get("/", getPosts);
 // router.put("/" , );
 router.delete("/", remove);
 
