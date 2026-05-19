0fdf19c9f80bcc34230a9dd5a9b1e0ae068c8e7b
Saad Salman <saadsalman708@gmail.com>
2026-05-20 02:35:54 +0500

    completed post.model.schema.js & created route.js

--- src/models/post.model.schema.js
+++ src/models/post.model.schema.js
@@ -1,9 +1,21 @@
-import mongoose from "mongoose";
+import mongoose, { model } from "mongoose";
 
 const postSchema = new mongoose.Schema(
     {
         title: {
-            
+            type: String,
+            required: true
+        },
+        description: {
+            type: String
+        },
+        imageUrl: {
+            type: String
         }
+    },
+    {
+        timestamps: true
     }
 );
+
+export default mongoose.model("post" , postSchema);
