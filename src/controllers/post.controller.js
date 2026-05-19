935d46b7d77e9eb6f0470f481e346fa18d337981
Saad Salman <saadsalman708@gmail.com>
2026-05-20 03:14:23 +0500

    fixed the bug & improved readablity

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,49 +1,44 @@
-import { creatPost, getAllPost, removePost  } from "../services/post.services";
+import { creatPost, getAllPost, removePost } from "../services/post.services";
 
-const create = async  ( req, res ) => {
+const create = async (req, res) => {
   try {
     const post = await creatPost(req.body);
     res.status(200).json({
       message: "New Post Created Successfully!",
-      post
-    })
+      post,
+    });
   } catch (error) {
-      res.status(500).json({
-        message: error.message
-      })
-    }
-}
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
-const getPosts = async  ( ) => {
-    try {
-        const posts = await getAllPost();
-        res.status(200).json({
-            message: "All Posts Fetched Successfully!",
-            posts
-        })
-    } catch (error) {
-        res.status(500).json({
-          message: error.message
-        })
-    }
-}
+const getPosts = async () => {
+  try {
+    const posts = await getAllPost();
+    res.status(200).json({
+      message: "All Posts Fetched Successfully!",
+      posts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
-const remove = async ( req , res ) => {
-    try {
-        await removePost(req.params.id);
-        res.status(200).json({
-            message: "Post deleted Successfully!"
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        })
-    }
-}
+const remove = async (req, res) => {
+  try {
+    await removePost(req.params.id);
+    res.status(200).json({
+      message: "Post deleted Successfully!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
-
-
-
-
-
-export {post,getPosts,remove};
+export { create, getPosts, remove };
